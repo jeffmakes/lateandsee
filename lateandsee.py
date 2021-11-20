54ecd39bf027bfa92ad066fe3b70bf58e54fd440
Jeff Gough <j@jeffreygough.com>
2021-11-20 23:14:42 +0000

    Add feature to write measurements to a file. The input and output file can be the same, so the tool can load an existing file and pick up where it left off, loading the last, e.g., 24hrs of data into the data buffer

--- lateandsee.py
+++ lateandsee.py
@@ -11,25 +11,35 @@ class LateAndSee:
         self.data_len = data_len       # number of measurements to store in buffer for live plotting
         self.data = []
         self.m = Measure()
+        self.outfile = None
         if (load_from_filename):
             with open(load_from_filename, "r") as f:
                 fdata = f.read().splitlines()
                 self.data = fdata[(-1*self.data_len):]
                 f.close()
 
+        if (write_to_filename):
+            self.outfile = open(write_to_filename, "a")
+
+    def __exit__(self):
+        if not self.outfile.closed:
+            self.outfile.flush()
+            self.outfile.close()
+
     def take_measurement(self):
         result = self.m.measure() 
-        #print(result)
         self.data.append(result)
 
         if len(self.data) > self.data_len:
             self.data.pop(0)
 
+        if (self.outfile):
+            self.outfile.write(result+'\n')
+            self.outfile.flush()
+
         self.print_data()
 
     def measurement_timer(self):
-        #global next_call
-        #global interval
         print (datetime.now())
         self.take_measurement()
 
@@ -41,7 +51,7 @@ class LateAndSee:
             print(d)
         
 if (__name__ == "__main__"):
-    l = LateAndSee(load_from_filename="data.csv")
+    l = LateAndSee(load_from_filename="data.csv", write_to_filename="data.csv")
     #start = datetime.today() 
     #plot_interval = timedelta(minutes=5)
     #plot_duration = timedelta(hours=24) 
