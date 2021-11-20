c6e7fb9520e5c22215946e895a0892e22a7f29ff
Jeff Gough <j@jeffreygough.com>
2021-11-20 17:14:20 +0000

    Produce two sub plots. Explicitly format the date values

--- plot.py
+++ plot.py
@@ -1,6 +1,6 @@
 #!/bin/python3
 import matplotlib.pyplot as plt
-from matplotlib.dates import AutoDateLocator, DateFormatter
+from matplotlib.dates import AutoDateLocator, DateFormatter, HourLocator
 from datetime import datetime
 
 if (__name__ == "__main__"):
@@ -10,23 +10,36 @@ if (__name__ == "__main__"):
     
     xvalues = []
     pingtimes = []
+    dltimes = []
+    speeds = []
     for l in data:
         d = l.split()
         timestamp = float(d[0])
         xvalues.append(datetime.fromtimestamp(timestamp))
         pingtimes.append(float(d[2]))
+        dltimes.append(float(d[5]))
+        speeds.append(float(d[7]))
 
-    fig, ax = plt.subplots()  # Create a figure containing a single axes.
-    ax.scatter(xvalues, pingtimes)  # Plot some data on the axes.
+    fig, ax = plt.subplots(2, 1, sharex=False)  # Create a figure containing a single axes.
+    ax[0].plot(xvalues, pingtimes)  # Plot some data on the axes.
+    #locator = AutoDateLocator()
+    locator = HourLocator(interval=3)
+    ax[0].xaxis.set_major_locator(locator)
+    ax[0].xaxis.set_major_formatter(DateFormatter("%Y-%m-%d %H:%M:%S"))
+    locator = HourLocator(interval=1) 
+    ax[0].xaxis.set_minor_locator(locator)
+    
+    ax[0].set_ylabel("Ping time ms")
+    ax[0].set(ylim=(5, 40))
+    labels = ax[0].get_xticklabels()
+    plt.setp(labels, rotation=45, horizontalalignment='right')
+    
+    ax[1].plot(xvalues, dltimes)  # Plot some data on the axes.
+    ax[1].set_ylabel("Download time s")
     #plt.gcf().autofmt_xdate()
-    labels = ax.get_xticklabels()
-    plt.setp(labels, rotation=45)
-    locator = AutoDateLocator()
-    ax.xaxis.set_major_locator(locator)
-    ax.xaxis.set_major_formatter(DateFormatter("%Y-%m-%d %H:%M:%S"))
     #ax.xaxis.set_major_formatter(AutoDateFormatter(locator))
-    fig.autofmt_xdate()
-    ax.set(ylim=(5, 40))
+    #fig.autofmt_xdate()
+    ax[1].set(ylim=(5, 40))
     plt.show()
     fig.savefig("out.png", transparent=False, dpi=80)
 
