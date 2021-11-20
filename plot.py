0c340c90355eecd9a31a00741e8689a2bd2bbc9c
Jeff Gough <j@jeffreygough.com>
2021-11-20 19:57:21 +0000

    Plots formatted nicely. Writing out to PNG at correct resolution

--- plot.py
+++ plot.py
@@ -1,5 +1,6 @@
 #!/bin/python3
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
 from matplotlib.dates import AutoDateLocator, DateFormatter, HourLocator
 from datetime import datetime
 
@@ -8,39 +9,49 @@ if (__name__ == "__main__"):
         data = f.readlines()
         f.close()
     
+
     xvalues = []
     pingtimes = []
     dltimes = []
-    speeds = []
+    dlspeeds = []
     for l in data:
         d = l.split()
         timestamp = float(d[0])
         xvalues.append(datetime.fromtimestamp(timestamp))
         pingtimes.append(float(d[2]))
         dltimes.append(float(d[5]))
-        speeds.append(float(d[7]))
+        dlspeeds.append(float(d[7]))
 
-    fig, ax = plt.subplots(2, 1, sharex=True)  # Create a figure containing a single axes.
-    ax[0].plot(xvalues, pingtimes)  # Plot some data on the axes.
-    #locator = AutoDateLocator()
+    fig, ax = plt.subplots(3, 1, sharex=True)  # Create a figure containing a single axes.
+    
+    ax[0].plot(xvalues, pingtimes, color="#57d5ff")  # Plot some data on the axes.
     locator = HourLocator(interval=3)
-    ax[1].xaxis.set_major_locator(locator)
-    ax[1].xaxis.set_major_formatter(DateFormatter("%Y-%m-%d %H:%M:%S"))
+    ax[0].xaxis.set_major_locator(locator)
+    ax[0].xaxis.set_major_formatter(DateFormatter("%Y-%m-%d %H:%M:%S"))
     locator = HourLocator(interval=1) 
-    ax[1].xaxis.set_minor_locator(locator)
+    ax[0].xaxis.set_minor_locator(locator)
     
     ax[0].set_ylabel("Ping time ms")
     ax[0].set(ylim=(5, 100))
-    labels = ax[1].get_xticklabels()
+    labels = ax[2].get_xticklabels()
     plt.setp(labels, rotation=45, horizontalalignment='right')
     
-    ax[1].plot(xvalues, dltimes)  # Plot some data on the axes.
+    ax[1].plot(xvalues, dltimes, "#ffa136")  # Plot some data on the axes.
     ax[1].set_ylabel("Download time s")
-    #plt.gcf().autofmt_xdate()
-    #ax.xaxis.set_major_formatter(AutoDateFormatter(locator))
-    #fig.autofmt_xdate()
-    ax[1].set(ylim=(5, 40))
-    plt.show()
-    fig.savefig("out.png", transparent=False, dpi=80)
+    ax[1].set(ylim=(0, 40))
+
+    ax[2].plot(xvalues, dlspeeds, "#2affac")
+    ax[2].set_ylabel("Download speed Mb/s")
+    ax[2].set(ylim=(0, 5))
+    
+#    plt.show()
+    #figure(num=1, figsize = (10, 30), dpi = 100) 
+    size_px = (1600, 1000)
+    dpi = 100 
+    fig.set_dpi(dpi)
+    fig.set_size_inches(size_px[0]/dpi, size_px[1]/dpi)
+    plt.subplots_adjust(top=0.9, bottom=0.2)
+    fig.savefig("out.png", transparent=False)
+
 
 
