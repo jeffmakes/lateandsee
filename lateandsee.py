b45f6eb37c7fa939bd60785e17832639c763b45e
Jeff Gough <j@jeffreygough.com>
2021-11-20 23:43:18 +0000

    Integrate the plotter into the main app

--- lateandsee.py
+++ lateandsee.py
@@ -2,16 +2,18 @@
 from measure import Measure
 import threading, time
 from datetime import datetime, timedelta
-
+from plot import Plot
 
 class LateAndSee:
-    def __init__(self, interval=5, data_len=10, load_from_filename=None, write_to_filename=None):
+    def __init__(self, interval=5, data_len=10, load_from_filename=None, write_to_filename=None, plot_filename=None):
         self.next_call = time.time()
         self.interval = interval        # interval between measurements in seconds
         self.data_len = data_len       # number of measurements to store in buffer for live plotting
         self.data = []
         self.m = Measure()
         self.outfile = None
+        self.plotter = None
+
         if (load_from_filename):
             with open(load_from_filename, "r") as f:
                 fdata = f.read().splitlines()
@@ -20,6 +22,9 @@ class LateAndSee:
 
         if (write_to_filename):
             self.outfile = open(write_to_filename, "a")
+
+        if (plot_filename):
+            self.plotter = Plot(plot_filename)
 
     def __exit__(self):
         if not self.outfile.closed:
@@ -37,7 +42,11 @@ class LateAndSee:
             self.outfile.write(result+'\n')
             self.outfile.flush()
 
+        if (self.plotter):
+            self.plotter.plot(self.data)
+
         self.print_data()
+
 
     def measurement_timer(self):
         print (datetime.now())
@@ -51,7 +60,7 @@ class LateAndSee:
             print(d)
         
 if (__name__ == "__main__"):
-    l = LateAndSee(load_from_filename="data.csv", write_to_filename="data.csv")
+    l = LateAndSee(load_from_filename="data.csv", write_to_filename="data.csv", plot_filename="out.png")
     #start = datetime.today() 
     #plot_interval = timedelta(minutes=5)
     #plot_duration = timedelta(hours=24) 
