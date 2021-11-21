aa464e065f6d8a4a2d4d44e754494ec61b5085f1
Jeff Gough <j@jeffreygough.com>
2021-11-21 16:45:51 +0000

    Add MeasureResult type, refactor to return result object rather than string. Stop things exploding when connection is down

--- measure.py
+++ measure.py
@@ -11,23 +11,23 @@ class Pinger:
     def ping(self):
         cp = run(["ping", "-c 1", "-D", self.host], capture_output=True)
         ret = cp.returncode
+        pingtime = None
 
         if (ret == 1):
             print("Timeout")
-            return PingResult(None, None, None, ret)
         
         elif (ret == 2):
             print("Host name not known")
-            return PingResult(None, None, None, ret)
             
         elif (ret == 0):
             response = cp.stdout.splitlines()[1].decode("utf-8")
-            timestamp = float(response[response.index('[')+1:response.index(']')]) #extract timestamp from between []'s
+            #timestamp = float(response[response.index('[')+1:response.index(']')]) #extract timestamp from between []'s
             pingtime = response.split("time=")[1].split()[0]
-            return PingResult(self.host, timestamp, pingtime, ret)
         
         else:
             print("Unknown error")
+
+        return PingResult(self.host, time(), pingtime, ret)
 
 class PingResult:
      
@@ -50,6 +50,8 @@ class Downloader:
     def download(self):
         cp = run(["curl", self.target, "--output", "/dev/null", "--max-time", str(self.timeout), "--write-out", "%{time_total} %{speed_download}"], capture_output=True)
         ret = cp.returncode
+        dltime = None
+        speed = None
         
         if (ret == 6):
             print("Host name not known")
@@ -57,7 +59,7 @@ class Downloader:
         elif (ret == 0):
             response = cp.stdout.splitlines()[0].decode("utf-8")
             dltime = response.split()[0]
-            speed = response.split()[1]
+            speed = float(response.split()[1])/(1024*1024)
 
         else:
             print("Unknown error")
@@ -71,12 +73,31 @@ class DownloadResult:
         self.target = target
         self.timestamp = timestamp
         self.dltime = dltime
-        self.speed = float(speed)/(1024*1024)
+        self.speed = speed
         self.returncode = returncode
         self.isotime = datetime.fromtimestamp(timestamp).replace(microsecond=0).isoformat()
 
     def __str__(self):
         return "{} {} {} s {:.3f} Mb/s {} {}".format(self.timestamp, self.isotime, self.dltime, self.speed, self.returncode, self.target)
+
+class MeasureResult:
+    def __init__(self, timestamp, ping_returncode, ping_time, dl_returncode, dl_time, dl_speed, ping_host, dl_target):
+        self.timestamp = timestamp
+        self.isotime = datetime.fromtimestamp(timestamp).replace(microsecond=0).isoformat()
+        self.ping_returncode = ping_returncode
+        self.ping_time = ping_time
+        self.dl_returncode = dl_returncode
+        self.dl_time = dl_time
+        self.dl_speed = dl_speed
+        self.ping_host = ping_host
+        self.dl_target = dl_target
+
+    def __str__(self):
+        try:
+            s = "{:.3f} {} {} ms {} {} s {:.3f} Mb/s {} {} {}".format(self.timestamp, self.isotime, self.ping_time, self.ping_returncode, self.dl_time, self.dl_speed, self.dl_returncode, self.ping_host, self.dl_target)
+        except:
+            s = "" 
+        return s
 
 class Measure():
 
@@ -88,9 +109,8 @@ class Measure():
         p = self.pinger.ping()
         d = self.downloader.download()
         timestamp = time()
-        isotime = datetime.fromtimestamp(timestamp).replace(microsecond=0).isoformat()
 
-        result = "{:.3f} {} {} ms {} {} s {:.3f} Mb/s {} {} {}".format(timestamp, isotime, p.pingtime, p.returncode, d.dltime, d.speed, d.returncode, p.host, d.target)
+        result = MeasureResult(timestamp, p.returncode, p.pingtime, d.returncode, d.dltime, d.speed, p.host, d.target)
         return result 
 
 
